0628702204e1d15eb76a019b95da7a64a143d9be
Bruno Perel <bruno@klarity.org>
2019-02-28 14:40:05 +0100

    Update dependencies Add contour detection

--- app/src/main/java/com/example/akshika/opencvtest/MainActivity.java
+++ app/src/main/java/com/example/akshika/opencvtest/MainActivity.java
@@ -9,7 +9,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
@@ -17,25 +16,24 @@ import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
-import org.opencv.core.DMatch;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Scalar;
 import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
-import org.opencv.features2d.Features2d;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -43,17 +41,16 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     private static final String TAG = "OCVSample::Activity";
     private static final int REQUEST_PERMISSION = 100;
-    private int w, h;
+    JavaCameraView javaCameraView;
+    Mat imgRGBA, imgGray, imgCanny;
     private CameraBridgeViewBase mOpenCvCameraView;
     TextView tvName;
-    Scalar RED = new Scalar(255, 0, 0);
-    Scalar GREEN = new Scalar(0, 255, 0);
     FeatureDetector detector;
     DescriptorExtractor descriptor;
     DescriptorMatcher matcher;
-    Mat descriptors2,descriptors1;
+    Mat descriptors1;
     Mat img1;
-    MatOfKeyPoint keypoints1,keypoints2;
+    MatOfKeyPoint keypoints1;
 
     static {
         if (!OpenCVLoader.initDebug())
@@ -124,18 +121,17 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_PERMISSION);
         }
 
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
+        mOpenCvCameraView = findViewById(R.id.tutorial1_activity_java_surface_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
-        tvName = (TextView) findViewById(R.id.text1);
+        tvName = findViewById(R.id.text1);
 
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+        if (javaCameraView != null) javaCameraView.disableView();
     }
 
     @Override
@@ -156,63 +152,64 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
             mOpenCvCameraView.disableView();
     }
 
+    @Override
     public void onCameraViewStarted(int width, int height) {
-        w = width;
-        h = height;
+        imgRGBA = new Mat(height, width, CvType.CV_8UC4);
+        imgGray = new Mat(height, width, CvType.CV_8UC1);
+        imgCanny = new Mat(height, width, CvType.CV_8UC1);
     }
 
+    @Override
     public void onCameraViewStopped() {
+        imgRGBA.release();
     }
 
-    public Mat recognize(Mat aInputFrame) {
-
-        Imgproc.cvtColor(aInputFrame, aInputFrame, Imgproc.COLOR_RGB2GRAY);
-        descriptors2 = new Mat();
-        keypoints2 = new MatOfKeyPoint();
-        detector.detect(aInputFrame, keypoints2);
-        descriptor.compute(aInputFrame, keypoints2, descriptors2);
-
-        // Matching
-        MatOfDMatch matches = new MatOfDMatch();
-        if (img1.type() == aInputFrame.type()) {
-            matcher.match(descriptors1, descriptors2, matches);
-        } else {
-            return aInputFrame;
-        }
-        List<DMatch> matchesList = matches.toList();
-
-        Double max_dist = 0.0;
-        Double min_dist = 100.0;
-
-        for (int i = 0; i < matchesList.size(); i++) {
-            Double dist = (double) matchesList.get(i).distance;
-            if (dist < min_dist)
-                min_dist = dist;
-            if (dist > max_dist)
-                max_dist = dist;
-        }
-
-        LinkedList<DMatch> good_matches = new LinkedList<DMatch>();
-        for (int i = 0; i < matchesList.size(); i++) {
-            if (matchesList.get(i).distance <= (1.5 * min_dist))
-                good_matches.addLast(matchesList.get(i));
-        }
-
-        MatOfDMatch goodMatches = new MatOfDMatch();
-        goodMatches.fromList(good_matches);
-        Mat outputImg = new Mat();
-        MatOfByte drawnMatches = new MatOfByte();
-        if (aInputFrame.empty() || aInputFrame.cols() < 1 || aInputFrame.rows() < 1) {
-            return aInputFrame;
-        }
-        Features2d.drawMatches(img1, keypoints1, aInputFrame, keypoints2, goodMatches, outputImg, GREEN, RED, drawnMatches, Features2d.NOT_DRAW_SINGLE_POINTS);
-        Imgproc.resize(outputImg, outputImg, aInputFrame.size());
-
-        return outputImg;
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        imgRGBA = inputFrame.rgba();
+        return detectPaper();
     }
 
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        return recognize(inputFrame.rgba());
+    private Mat detectPaper() {
+        Imgproc.cvtColor(imgRGBA, imgGray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.Canny(imgGray, imgCanny, 75, 20);
+        //return imgCanny;
 
+        /* Book Code */
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+
+        Imgproc.findContours(imgCanny, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        if (contours.size() == 0) {
+            Log.i(TAG, "Não encontrei contornos!");
+            return imgRGBA;
+        }
+
+        // Encontra o contorno com maior área
+        int index = 0;
+        double maxim = Imgproc.contourArea(contours.get(0));
+        for (int contourIdx = 1; contourIdx < contours.size(); contourIdx++) {
+            double temp;
+            temp = Imgproc.contourArea(contours.get(contourIdx));
+
+            if (maxim < temp) {
+                maxim = temp;
+                index = contourIdx;
+            }
+        }
+
+        double peri = 0.02 * Imgproc.arcLength(new MatOfPoint2f(contours.get(index).toArray()), true);
+        MatOfPoint2f approx = new MatOfPoint2f();
+        Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(index).toArray()), approx, peri, true);
+
+        if (approx.size().height == 4 && Imgproc.contourArea(contours.get(index)) > 30000) {
+            Log.i(TAG, "Height: " + approx.size().height);
+            Log.i(TAG, "Width: " + approx.size().width);
+            Log.i(TAG, "Área: " + Imgproc.contourArea(contours.get(index)));
+            Imgproc.drawContours(imgRGBA, contours, index, new Scalar(0, 255, 0), 5);
+            return imgRGBA;
+        }
+
+        return imgRGBA;
     }
 }
